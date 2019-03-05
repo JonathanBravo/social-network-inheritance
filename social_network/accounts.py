2e91f227556e3aab08f87d9ad133c4dd32c191eb
Jonathan Flowers <jmflowers134@gmail.com>
2019-03-05 18:38:43 -0500

    Update accounts.py

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,23 @@
-
-class User(object):
-    def __init__(self, first_name, last_name, email):
-        pass
-
-    def add_post(self, post):
-        pass
-
+class User():
+    def __init__(self,first_name, last_name, email):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.email=email
+        self.posts=[]
+        self.following=[]
+        self.timeline=[]
+    
+    def add_post(self,post):
+        post.user = self
+        self.posts.append(post)
+    
+    def follow(self,user):
+        self.following.append(user)
+        
     def get_timeline(self):
-        pass
-
-    def follow(self, other):
-        pass
+        timeline = []
+        for each_user in self.following:
+            for each_post in each_user.posts:
+                timeline.append(each_post)
+        timeline.sort(key=lambda post: post.date_for_sorting)
+        return timeline
